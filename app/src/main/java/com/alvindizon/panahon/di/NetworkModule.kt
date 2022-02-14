1f607745396b52fc6127c976b60dd62bf4e9c643
Alvin Dizon <alvin.dizon91@gmail.com>
2022-02-14 10:33:24 +0800

    Add Hilt module for providing API
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ app/src/main/java/com/alvindizon/panahon/di/NetworkModule.kt
@@ -0,0 +1,68 @@
+package com.alvindizon.panahon.di
+
+
+import android.content.Context
+import android.net.ConnectivityManager
+import com.alvindizon.panahon.BuildConfig
+import com.alvindizon.panahon.data.networking.api.OpenWeatherApi
+import com.alvindizon.panahon.data.networking.interceptor.ConnectivityInterceptor
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val OPENWEATHER_URL = "https://api.openweathermap.org/"
+
+    @Provides
+    @Singleton
+    fun provideConnectivityManager(@ApplicationContext context: Context): ConnectivityManager {
+        return context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level =
+            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        return loggingInterceptor
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        connectivityInterceptor: ConnectivityInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .addInterceptor(connectivityInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit = Retrofit.Builder()
+        .baseUrl(OPENWEATHER_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(retrofit: Retrofit): OpenWeatherApi =
+        retrofit.create(OpenWeatherApi::class.java)
+}
