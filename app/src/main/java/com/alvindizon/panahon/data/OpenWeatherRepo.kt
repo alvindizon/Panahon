2fe4fb3463aa0c8b0ea1b986a5e75b8c254241e7
Alvin Dizon <alvin.dizon91@gmail.com>
2022-02-20 18:34:13 +0800

    Add repo code for getting location name from OpenWeather API
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/alvindizon/panahon/data/OpenWeatherRepo.kt
+++ app/src/main/java/com/alvindizon/panahon/data/OpenWeatherRepo.kt
@@ -2,12 +2,18 @@ package com.alvindizon.panahon.data
 
 import com.alvindizon.panahon.data.api.OpenWeatherApi
 import com.alvindizon.panahon.data.api.model.OneCallResponse
+import com.alvindizon.panahon.data.api.model.ReverseGeocodeResponse
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface OpenWeatherRepo {
 
     suspend fun getWeatherForLocation(latitude: String, longitude: String): OneCallResponse
+
+    suspend fun getLocationNameFromCoordinates(
+        latitude: String,
+        longitude: String
+    ): ReverseGeocodeResponse
 }
 
 @Singleton
@@ -16,6 +22,11 @@ class OpenWeatherRepoImpl @Inject constructor(private val api: OpenWeatherApi) :
         latitude: String,
         longitude: String
     ): OneCallResponse = api.getWeather(latitude, longitude, null, OPENWEATHER_UNIT)
+
+    override suspend fun getLocationNameFromCoordinates(
+        latitude: String,
+        longitude: String
+    ): ReverseGeocodeResponse = api.getLocationName(latitude, longitude)
 
     companion object {
         private const val OPENWEATHER_UNIT = "metric"
