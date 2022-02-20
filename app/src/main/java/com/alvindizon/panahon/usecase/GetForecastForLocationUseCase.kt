b17dd5fb9b15bc5ab722b97458674f74e3411141
Alvin Dizon <alvin.dizon91@gmail.com>
2022-02-20 18:38:49 +0800

    Add initial usecase code for getting location forecasts
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ app/src/main/java/com/alvindizon/panahon/usecase/GetForecastForLocationUseCase.kt
@@ -0,0 +1,28 @@
+package com.alvindizon.panahon.usecase
+
+import com.alvindizon.panahon.data.OpenWeatherRepo
+import com.alvindizon.panahon.ui.locations.LocationForecast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetForecastForLocationUseCase @Inject constructor(private val openWeatherRepo: OpenWeatherRepo) {
+
+    suspend fun execute(latitude: String, longitude: String): LocationForecast =
+        withContext(Dispatchers.Default) {
+            val reverseGeocodeDeferred =
+                async { openWeatherRepo.getLocationNameFromCoordinates(latitude, longitude) }
+            val oneCallDeferred =
+                async { openWeatherRepo.getWeatherForLocation(latitude, longitude) }
+            val reverseGeocodeResponse = reverseGeocodeDeferred.await()
+            val oneCallResponse = oneCallDeferred.await()
+            LocationForecast(
+                reverseGeocodeResponse[0].name,
+                oneCallResponse.current.weather[0].main,
+                oneCallResponse.current.temp.toString()
+            )
+        }
+}
