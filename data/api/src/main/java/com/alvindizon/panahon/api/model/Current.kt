84a6dd85dc2ca65a593e6ccc62c06bf5a18c0c94
Alvin Dizon <alvin.dizon91@gmail.com>
2022-05-31 12:52:24 +0800

    Create separate module that contains API code
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- data/api/src/main/java/com/alvindizon/panahon/api/model/Current.kt
+++ data/api/src/main/java/com/alvindizon/panahon/api/model/Current.kt
@@ -1,11 +1,11 @@
-package com.alvindizon.panahon.data.api.model
+package com.alvindizon.panahon.api.model
 
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Daily(
+data class Current(
     @Json(name = "clouds")
     val clouds: Int?,
     @Json(name = "dew_point")
@@ -13,35 +13,25 @@ data class Daily(
     @Json(name = "dt")
     val dt: Int?,
     @Json(name = "feels_like")
-    val feelsLike: FeelsLike?,
+    val feelsLike: Double?,
     @Json(name = "humidity")
     val humidity: Int?,
-    @Json(name = "moon_phase")
-    val moonPhase: Double?,
-    @Json(name = "moonrise")
-    val moonrise: Int?,
-    @Json(name = "moonset")
-    val moonset: Int?,
-    @Json(name = "pop")
-    val pop: Double?,
     @Json(name = "pressure")
     val pressure: Int?,
-    @Json(name = "rain")
-    val rain: Double?,
     @Json(name = "sunrise")
     val sunrise: Int?,
     @Json(name = "sunset")
     val sunset: Int?,
     @Json(name = "temp")
-    val temp: Temp?,
+    val temp: Double,
     @Json(name = "uvi")
     val uvi: Double?,
+    @Json(name = "visibility")
+    val visibility: Int?,
     @Json(name = "weather")
-    val weather: List<Weather>?,
+    val weather: List<Weather>,
     @Json(name = "wind_deg")
     val windDeg: Int?,
-    @Json(name = "wind_gust")
-    val windGust: Double?,
     @Json(name = "wind_speed")
     val windSpeed: Double?
 )
--- data/api/src/main/java/com/alvindizon/panahon/api/model/Daily.kt
+++ data/api/src/main/java/com/alvindizon/panahon/api/model/Daily.kt
@@ -1,11 +1,11 @@
-package com.alvindizon.panahon.data.api.model
+package com.alvindizon.panahon.api.model
 
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Current(
+data class Daily(
     @Json(name = "clouds")
     val clouds: Int?,
     @Json(name = "dew_point")
@@ -13,25 +13,35 @@ data class Current(
     @Json(name = "dt")
     val dt: Int?,
     @Json(name = "feels_like")
-    val feelsLike: Double?,
+    val feelsLike: FeelsLike?,
     @Json(name = "humidity")
     val humidity: Int?,
+    @Json(name = "moon_phase")
+    val moonPhase: Double?,
+    @Json(name = "moonrise")
+    val moonrise: Int?,
+    @Json(name = "moonset")
+    val moonset: Int?,
+    @Json(name = "pop")
+    val pop: Double?,
     @Json(name = "pressure")
     val pressure: Int?,
+    @Json(name = "rain")
+    val rain: Double?,
     @Json(name = "sunrise")
     val sunrise: Int?,
     @Json(name = "sunset")
     val sunset: Int?,
     @Json(name = "temp")
-    val temp: Double,
+    val temp: Temp?,
     @Json(name = "uvi")
     val uvi: Double?,
-    @Json(name = "visibility")
-    val visibility: Int?,
     @Json(name = "weather")
-    val weather: List<Weather>,
+    val weather: List<Weather>?,
     @Json(name = "wind_deg")
     val windDeg: Int?,
+    @Json(name = "wind_gust")
+    val windGust: Double?,
     @Json(name = "wind_speed")
     val windSpeed: Double?
 )
