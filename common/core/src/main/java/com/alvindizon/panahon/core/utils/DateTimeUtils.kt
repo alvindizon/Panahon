125fec04fafa0d0a60dfe75cbbd9c8ce4eb0bb1f
Alvin Dizon <alvin.dizon91@gmail.com>
2022-06-12 10:55:58 +0800

    Add utils for formatting timestamps to readable strings
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ common/core/src/main/java/com/alvindizon/panahon/core/utils/DateTimeUtils.kt
@@ -0,0 +1,15 @@
+package com.alvindizon.panahon.core.utils
+
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+private val DEFAULT_FORMAT_LOCALE = Locale.ENGLISH
+
+fun Long.convertTimestampToString(format: String, timezone: String?): String {
+    val instant = Instant.ofEpochSecond(this)
+    val df = DateTimeFormatter.ofPattern(format, DEFAULT_FORMAT_LOCALE)
+    val zoneId = timezone?.let { ZoneId.of(it) } ?: ZoneId.systemDefault()
+    return instant.atZone(zoneId).format(df).toString()
+}
