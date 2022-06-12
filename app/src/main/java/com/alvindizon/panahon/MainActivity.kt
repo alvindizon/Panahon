d9e0cee1c546f4adc05fa873314605b37a0d5556
Alvin Dizon <alvin.dizon91@gmail.com>
2022-06-12 10:59:31 +0800

    Navigate to detailed forecast screen on location item click
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/alvindizon/panahon/MainActivity.kt
+++ app/src/main/java/com/alvindizon/panahon/MainActivity.kt
@@ -7,19 +7,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.alvindizon.panahon.design.theme.PanahonTheme
+import com.alvindizon.panahon.details.ui.DetailsScreen
+import com.alvindizon.panahon.details.viewmodel.DetailsScreenViewModel
 import com.alvindizon.panahon.locations.ui.LocationsScreen
-import com.alvindizon.panahon.ui.search.SearchScreen
 import com.alvindizon.panahon.locations.viewmodel.LocationScreenViewModel
 import com.alvindizon.panahon.searchlocation.viewmodel.SearchLocationViewModel
+import com.alvindizon.panahon.ui.search.SearchScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 enum class Screens {
     Locations,
-    Search
+    Search,
+    Details
 }
 
 @AndroidEntryPoint
@@ -40,17 +45,39 @@ fun PanahonNavHost(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screens.Locations.name) {
         composable(Screens.Locations.name) {
             val viewModel = hiltViewModel<LocationScreenViewModel>()
-            LocationsScreen(viewModel,  stringResource(id = R.string.app_name)) {
-                navController.navigate(Screens.Search.name)
-            }
+            LocationsScreen(viewModel = viewModel,
+                title = stringResource(id = R.string.app_name),
+                onSearchIconClicked = {
+                    navController.navigate(Screens.Search.name)
+                },
+                onLocationClick = {
+                    navController.navigate(
+                        "${Screens.Details.name}/${it.name}/${it.latitude}/${it.longitude}"
+                    )
+                }
+            )
         }
         composable(Screens.Search.name) {
             val viewModel = hiltViewModel<SearchLocationViewModel>()
             SearchScreen(
-                viewModel,
+                viewModel = viewModel,
                 onUpButtonClicked = {
                     navController.popBackStack()
-                })
+                }
+            )
+        }
+        composable("${Screens.Details.name}/{location}/{latitude}/{longitude}", arguments = listOf(
+            navArgument("location") { type = NavType.StringType },
+            navArgument("latitude") { type = NavType.StringType },
+            navArgument("longitude") { type = NavType.StringType }
+        )) {
+            val viewModel = hiltViewModel<DetailsScreenViewModel>()
+            DetailsScreen(
+                viewModel = viewModel,
+                location = it.arguments!!.getString("location")!!,
+                latitude = it.arguments!!.getString("latitude")!!,
+                longitude = it.arguments!!.getString("longitude")!!
+            )
         }
     }
 }
--- feature/locations/src/main/java/com/alvindizon/panahon/locations/model/LocationForecast.kt
+++ feature/locations/src/main/java/com/alvindizon/panahon/locations/model/LocationForecast.kt
@@ -2,6 +2,8 @@ package com.alvindizon.panahon.locations.model
 
 data class LocationForecast(
     val name: String,
+    val latitude: String,
+    val longitude: String,
     val condition: String,
     val temperature: String,
     val icon: String
