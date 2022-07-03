4d22ff5d8f7368eff0ca6f9f073a2d1307d011c1
Alvin Dizon <alvin.dizon91@gmail.com>
2022-07-03 17:32:03 +0800

    Add boolean marking a location as home location
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- data/db/src/main/java/com/alvindizon/panahon/db/model/Location.kt
+++ data/db/src/main/java/com/alvindizon/panahon/db/model/Location.kt
@@ -3,4 +3,4 @@ package com.alvindizon.panahon.db.model
 import androidx.room.Entity
 
 @Entity(tableName = "savedLocations", primaryKeys = ["name"])
-data class Location(val name: String, val latitude: String, val longitude: String)
+data class Location(val name: String, val latitude: String, val longitude: String, val isHomeLocation: Boolean)
