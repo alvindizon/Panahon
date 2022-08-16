3937fd1ade6a173473e8406d7bd1ae43dddd1795
Alvin Dizon <alvin.dizon91@gmail.com>
2022-08-16 13:08:33 +0800

    Add Timber for logging
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/alvindizon/panahon/PanahonApp.kt
+++ app/src/main/java/com/alvindizon/panahon/PanahonApp.kt
@@ -2,7 +2,12 @@ package com.alvindizon.panahon
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class PanahonApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
