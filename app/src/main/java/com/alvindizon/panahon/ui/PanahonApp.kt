8e81397a98a6deef0bf1a520f4b2c3613c9d6092
Alvin Dizon <alvin.dizon91@gmail.com>
2022-08-28 18:23:07 +0800

    Move bottom bar boolean and current route to state holder
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/alvindizon/panahon/ui/PanahonApp.kt
+++ app/src/main/java/com/alvindizon/panahon/ui/PanahonApp.kt
@@ -4,14 +4,8 @@ import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.alvindizon.panahon.design.theme.PanahonTheme
-import com.alvindizon.panahon.home.navigation.HomeNavigation
 import com.alvindizon.panahon.navigation.PanahonNavHost
 
 
@@ -20,17 +14,14 @@ fun PanahonApp(
     activity: ComponentActivity,
     appState: PanahonAppState = rememberPanahonAppState()
 ) {
-    var showBottomBar by rememberSaveable{ mutableStateOf(true) }
-    val navBackStackEntry by appState.navController.currentBackStackEntryAsState()
-    showBottomBar = navBackStackEntry?.destination?.route != HomeNavigation.route
     PanahonTheme {
         Scaffold(
             modifier = Modifier.windowInsetsPadding(
                 WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical)
             ),
             bottomBar = {
-                if (showBottomBar) PanahonBottomNavBar(
-                    navController = appState.navController,
+                if (appState.shouldShowBottomBar) PanahonBottomNavBar(
+                    currentRoute = appState.currentRoute,
                     destinations = appState.topLevelDestinations,
                     onNavigateToDestination = appState::navigate
                 )
