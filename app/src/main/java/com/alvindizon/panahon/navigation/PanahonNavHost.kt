f399c070b6ac60c41ee716299efd01a5051d2ed4
Alvin Dizon <alvin.dizon91@gmail.com>
2022-08-28 20:48:17 +0800

    Add padding to whole composable so that last item is visible
    - ref: https://stackoverflow.com/questions/66573601/bottom-nav-bar-overlaps-screen-content-in-jetpack-compose
    
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/alvindizon/panahon/navigation/PanahonNavHost.kt
+++ app/src/main/java/com/alvindizon/panahon/navigation/PanahonNavHost.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.alvindizon.panahon.details.navigation.DetailsNavigation
@@ -17,44 +19,47 @@ import com.alvindizon.panahon.searchlocation.navigation.searchGraph
 
 @Composable
 fun PanahonNavHost(
+    modifier: Modifier = Modifier,
     navController: NavHostController,
     context: Context,
     startDestination: String = HomeNavigation.route,
     finishActivity: () -> Unit,
     popBackStack: () -> Unit,
 ) {
-    NavHost(navController = navController, startDestination = startDestination) {
-        homeGraph(
-            onLocationFound = {
-                navController.navigate(
-                    DetailsNavigation.createNavigationRoute(
-                        it.locationName, it.latitude, it.longitude
+    Box(modifier = modifier) {
+        NavHost(navController = navController, startDestination = startDestination) {
+            homeGraph(
+                onLocationFound = {
+                    navController.navigate(
+                        DetailsNavigation.createNavigationRoute(
+                            it.locationName, it.latitude, it.longitude
+                        )
                     )
-                )
-            },
-            onSnackbarButtonClick = {
-                context.startActivity(
-                    Intent(
-                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                        Uri.parse("package:${context.packageName}")
+                },
+                onSnackbarButtonClick = {
+                    context.startActivity(
+                        Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:${context.packageName}")
+                        )
                     )
-                )
-            },
-            onSearchLinkClick = { navController.navigate(SearchNavigation.route) },
-            onErrorOkBtnClick = finishActivity
-        )
-        locationsGraph(
-            onLocationClick = {
-                navController.navigate(
-                    DetailsNavigation.createNavigationRoute(
-                        it.name, it.latitude, it.longitude
+                },
+                onSearchLinkClick = { navController.navigate(SearchNavigation.route) },
+                onErrorOkBtnClick = finishActivity
+            )
+            locationsGraph(
+                onLocationClick = {
+                    navController.navigate(
+                        DetailsNavigation.createNavigationRoute(
+                            it.name, it.latitude, it.longitude
+                        )
                     )
-                )
-            },
-            onUpButtonClicked = popBackStack
-        )
-        searchGraph(onUpButtonClicked = popBackStack)
-        detailsGraph(onSearchIconClick = { navController.navigate(SearchNavigation.route) })
+                },
+                onUpButtonClicked = popBackStack
+            )
+            searchGraph(onUpButtonClicked = popBackStack)
+            detailsGraph(onSearchIconClick = { navController.navigate(SearchNavigation.route) })
+        }
     }
 
 }
