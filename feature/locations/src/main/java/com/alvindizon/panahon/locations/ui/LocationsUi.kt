1c966a5c82adb7f13251522000cdc923b1b11969
Alvin Dizon <alvin.dizon91@gmail.com>
2023-02-05 21:25:16 +0800

    Change location list item shape, use local drawable weather icons
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- feature/locations/src/main/java/com/alvindizon/panahon/locations/ui/LocationsUi.kt
+++ feature/locations/src/main/java/com/alvindizon/panahon/locations/ui/LocationsUi.kt
@@ -5,30 +5,57 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.*
+import androidx.compose.material.rememberDismissState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.alvindizon.panahon.design.R
 import com.alvindizon.panahon.design.components.LoadingScreen
 import com.alvindizon.panahon.design.theme.PanahonTheme
+import com.alvindizon.panahon.design.utils.IconUtils
 import com.alvindizon.panahon.locations.model.LocationForecast
 import com.alvindizon.panahon.locations.viewmodel.LocationScreenUiState
 import com.alvindizon.panahon.locations.viewmodel.LocationScreenViewModel
@@ -212,7 +239,7 @@ fun LocationsListItem(
     onLocationClick: (LocationForecast) -> Unit,
 ) {
     Card(
-        shape = RoundedCornerShape(5.dp),
+        shape = RectangleShape,
         modifier = Modifier
             .clickable { onLocationClick(locationForecast) }
             .fillMaxWidth()
@@ -257,7 +284,7 @@ fun WeatherIconAndTemperature(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
-            painter = rememberAsyncImagePainter(model = "https://openweathermap.org/img/wn/$icon@2x.png"),
+            painter = painterResource(id = IconUtils.getWeatherIconRes(icon)),
             contentDescription = "weather icon",
             modifier = Modifier
                 .size(72.dp)
