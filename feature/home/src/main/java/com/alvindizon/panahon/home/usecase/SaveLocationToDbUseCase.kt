806f28c236d8f192f44c84e57258ab3f27c00c43
Alvin Dizon <alvin.dizon91@gmail.com>
2023-10-22 16:57:19 +0800

    Use IO dispatcher for DB operations
    - For some reason, adding suspend keyword to DAOs causes an error when
      building. Thus we need to specify the usage of the IO dispatcher
    explicitly.
    
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- feature/home/src/main/java/com/alvindizon/panahon/home/usecase/SaveLocationToDbUseCase.kt
+++ feature/home/src/main/java/com/alvindizon/panahon/home/usecase/SaveLocationToDbUseCase.kt
@@ -2,21 +2,32 @@ package com.alvindizon.panahon.home.usecase
 
 import com.alvindizon.panahon.home.data.HomeRepository
 import com.alvindizon.panahon.home.model.CurrentLocation
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@Singleton
+@ViewModelScoped
 class SaveLocationToDbUseCase @Inject constructor(private val repo: HomeRepository) {
 
     suspend fun execute(name: String, latitude: String, longitude: String) {
-        val locationToBeSaved = CurrentLocation(name, latitude, longitude)
-        // get home location
-        val currentLocation = repo.getHomeLocation()
-        // if another home location exists and it is not the same as the currently detected location,
-        // remove existing one as current home location by updating its entry
-        if (currentLocation != null && locationToBeSaved != currentLocation) {
-            repo.updateDbLocation(currentLocation.locationName, currentLocation.latitude, currentLocation.longitude, false)
+        withContext(Dispatchers.IO) {
+            val locationToBeSaved = CurrentLocation(name, latitude, longitude)
+            // get home location
+            val currentLocation = repo.getHomeLocation()
+            // if another home location exists and it is not the same as the currently detected location,
+            // remove existing one as current home location by updating its entry
+            if (currentLocation != null && locationToBeSaved != currentLocation) {
+                repo.updateDbLocation(
+                    currentLocation.locationName,
+                    currentLocation.latitude,
+                    currentLocation.longitude,
+                    false
+                )
+            }
+            repo.saveHomeLocationToDatabase(name, latitude, longitude)
         }
-        repo.saveHomeLocationToDatabase(name, latitude, longitude)
     }
 }
