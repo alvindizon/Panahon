04c3279ce2182c7e0b9292695bddacf1bf4da763
Alvin Dizon <alvin.dizon91@gmail.com>
2023-10-22 17:38:04 +0800

    Actually use compose navigation library and use animation APIs again
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- feature/locations/src/main/java/com/alvindizon/panahon/locations/navigation/LocationsNavigation.kt
+++ feature/locations/src/main/java/com/alvindizon/panahon/locations/navigation/LocationsNavigation.kt
@@ -1,5 +1,12 @@
 package com.alvindizon.panahon.locations.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -22,7 +29,27 @@ fun NavGraphBuilder.locationsGraph(
 ) {
     navigation(
         route = LocationsNavigation.route,
-        startDestination = LocationsNavigation.destination
+        startDestination = LocationsNavigation.destination,
+        enterTransition = {
+            fadeIn(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.End
+            )
+        },
+        exitTransition = {
+            fadeOut(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseOut),
+                towards = AnimatedContentTransitionScope.SlideDirection.Start
+            )
+        }
     ) {
         composable(route = LocationsNavigation.destination) {
             LocationsScreen(
--- feature/searchlocation/src/main/java/com/alvindizon/panahon/searchlocation/navigation/SearchNavigation.kt
+++ feature/searchlocation/src/main/java/com/alvindizon/panahon/searchlocation/navigation/SearchNavigation.kt
@@ -1,5 +1,12 @@
 package com.alvindizon.panahon.searchlocation.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -18,7 +25,29 @@ fun NavGraphBuilder.searchGraph(
     onSearchResultClicked: (SearchResult) -> Unit,
     onLocationFound: (CurrentLocation) -> Unit
 ) {
-    composable(route = SearchNavigation.route) {
+    composable(
+        route = SearchNavigation.route,
+        enterTransition = {
+            fadeIn(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Up
+            )
+        },
+        exitTransition = {
+            fadeOut(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseOut),
+                towards = AnimatedContentTransitionScope.SlideDirection.Down
+            )
+        }
+    ) {
         SearchScreen(
             viewModel = hiltViewModel(),
             onUpButtonClicked = onUpButtonClicked,
