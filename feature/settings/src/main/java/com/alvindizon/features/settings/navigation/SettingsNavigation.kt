2f980cdedd085d8f2a273328f61cb6931cdf923f
Alvin Dizon <alvin.dizon91@gmail.com>
2023-10-22 17:38:04 +0800

    Actually use compose navigation library and use animation APIs again
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- feature/settings/src/main/java/com/alvindizon/features/settings/navigation/SettingsNavigation.kt
+++ feature/settings/src/main/java/com/alvindizon/features/settings/navigation/SettingsNavigation.kt
@@ -1,5 +1,12 @@
 package com.alvindizon.features.settings.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -17,7 +24,28 @@ fun NavGraphBuilder.settingsGraph(
 ) {
     navigation(
         route = SettingsNavigation.route,
-        startDestination = SettingsNavigation.destination
+        startDestination = SettingsNavigation.destination,
+        enterTransition = {
+            fadeIn(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Start
+            )
+        },
+        exitTransition = {
+            fadeOut(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseOut),
+                towards = AnimatedContentTransitionScope.SlideDirection.End
+            )
+        }
+
     ) {
         composable(
             route = SettingsNavigation.destination
